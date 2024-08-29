30e2d619e99247408068da16e456779a1d07eebb
deconey87 <deconey87@gmail.com>
2024-08-29 18:59:04 +0900

    feat: 🎸 15.2 Derefトレイトの実装

--- smartpointer/src/main.rs
+++ smartpointer/src/main.rs
@@ -26,4 +26,59 @@ fn main() {
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // 参照外しの基本
+    let x = 5;
+    let y = &x; // yは &i32（i32型への参照）
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // 参照外しして、値を見に行く
+
+    // 参照ではなくBoxを使う
+    let z = Box::new(x);
+    // 参照外しして、値を見に行く
+    assert_eq!(5, *z);
+
+    // これは z = 5 を表示する。
+    // Displayトレイトによって、暗黙的に *z になっている
+    println!("z = {}", z);
+
+    // 独自実装のスマートポインタ MyBox を使用する
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    // ↑ のコードはRustによって暗黙的に以下のように処理される
+    // assert_eq!(5, *(y.deref()));
+
+    // deref coercion
+    // Derefトレイトを実装している型への参照を、別の型の参照に変換する
+    hello("jack"); // 普通の呼び出し &str
+    let m = MyBox::new(String::from("Rust"));
+    // &m は &MyBox<String>だが、 MyBoxがDerefを実装しているので、 deref() が呼び出されて &String になる
+    // そして、StringもDerefが実装されているので &str になって、関数が求める型と一致する
+    hello(&m);
+}
+
+// Boxの独自実装
+use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// スマートポインタはDefefを実装する必要がある
+// これにより、普通の参照のように扱えるようになる
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("hello, {}", name);
 }
